94def60ec19d9071841be9063bbfccce57ce1cb3
Ziad Abdelfattah <1901658@eng.asu.edu.eg>
2024-08-28 00:04:38 +0300

    Add files via upload

--- Number_Guess.py
+++ Number_Guess.py
@@ -1,44 +1,61 @@
 import random
 import math
 
+def getint(x):
+    temp = 0
+
+    while True:
+        try:
+            temp = int(x)
+            break
+
+        except ValueError:
+            x = input("That's not an integer. Please try again: ")
+
+    return temp
+
 print("Welcome to Number Guessing Game! \n")
 print("We will play with integers only \n")
 
-lower_bound = int(input("Enter the lower bound: "))
-upper_bound = int(input("Enter the upper bound: "))
+lower_bound = getint(input("Enter the lower bound: "))
+upper_bound = getint(input("Enter the upper bound: "))
+
+if (upper_bound < lower_bound):
+    upper_bound, lower_bound = lower_bound, upper_bound
 
 answer = random.randint(lower_bound, upper_bound)
-
 chances = math.ceil(math.log2(upper_bound - lower_bound +1))
 
-guess = int(input("Guess the number \n"))
 
 guess_no = 0
+did_guess = False
 
 while guess_no < chances:
 
-    if (guess < lower_bound) or (guess > upper_bound):
+    guess = getint(input("Guess the number: "))
+    
 
-        print(f"Out of bounds pleaase guess within range {lower_bound} to {upper_bound} \n")
-        guess = int(input("Guess again: \n"))
-        guess_no += 1
+    if ( (guess < lower_bound) or (guess > upper_bound) ):
+
+        print(f"Out of bounds please guess within range {lower_bound} to {upper_bound} \n")
+        #guess = getint(input("Guess again: \n"))
 
     elif guess > answer:
         
         print("Guess was too high ;) \n")
-        guess = int(input("Guess again: \n"))
+        #guess = getint(input("Guess again: \n"))
         guess_no += 1
     
     elif guess < answer:
         
         print("Guess was too low ;) \n")
-        guess = int(input("Guess again: \n"))
+        #guess = getint(input("Guess again: \n"))
         guess_no += 1
 
     elif guess == answer:
         
-        +guess_no
         did_guess = True
+        guess_no += 1
         break
 
 
